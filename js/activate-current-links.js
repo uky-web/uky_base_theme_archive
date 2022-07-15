fc6b2d99908a050869efbee77130c33f900b875a
Andy Hebrank <ahebrank@gmail.com>
2022-07-15 15:55:39 +0000

    Resolve "activate-current-links.js fails strict check because of uninitialized variables"

--- js/activate-current-links.js
+++ js/activate-current-links.js
@@ -5,8 +5,8 @@ var $ = jQuery;
 Drupal.behaviors.activate_current_links = {
   attach: function attach(context, settings) {
     $('a').each(function () {
-      $a = $(this);
-      url = $a.attr('href');
+      var $a = $(this);
+      var url = $a.attr('href');
       if (url == window.location.href || url == window.location.pathname) {
         $a.addClass('is-active');
       }
